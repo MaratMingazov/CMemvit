e21989ea852416cfd97363d1c38a222a5ff2924f
marat.mingazov <maratmingazov@gmail.com>
2016-05-05 14:26:23 +0300

    master

--- src/ru/innopolis/lips/memvit/ActivationRecord.java
+++ src/ru/innopolis/lips/memvit/ActivationRecord.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2015.
  * This file is part of Memvit.
- 
+ part1
  * Memvit is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
