beddf469ca10600e95afa3d329e14048fe128f48
marat.mingazov <maratmingazov@gmail.com>
2016-05-05 14:40:25 +0300

    project refactor

--- src/ru/innopolis/lips/memvit/ActivationRecord.java
+++ src/ru/innopolis/lips/memvit/ActivationRecord.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2015.
  * This file is part of Memvit.
- part1
+
  * Memvit is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,6 @@
 
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- * part2
  *******************************************************************************/
 
 package ru.innopolis.lips.memvit;
